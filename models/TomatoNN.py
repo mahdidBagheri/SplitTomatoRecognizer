1c3714513e58bb7ce735031e20b2743c549619d3
mahdi <mahdid.m.2000@gmail.com>
2023-03-02 16:17:21 +0330

    train exp - last one was underfit

--- models/TomatoNN.py
+++ models/TomatoNN.py
@@ -5,24 +5,21 @@ import torch
 class TomatoNN(nn.Module):
     def __init__(self):
         super(TomatoNN, self).__init__()
-        self.conv1 = self.conv_block(n=1, nIn=3, nOut=64)
-        self.conv2 = self.conv_block(n=1, nIn=64, nOut=128)
+        self.conv1 = self.conv_block(n=1, nIn=3, nOut=128)
         self.pool3 = nn.MaxPool2d(kernel_size=(4,4))
 
-        self.conv4 = self.conv_block(n=1, nIn=128, nOut=256)
-        self.conv5 = self.conv_block(n=1, nIn=256, nOut=512)
+        self.conv4 = self.conv_block(n=1, nIn=128, nOut=512)
         self.pool6 = nn.MaxPool2d(kernel_size=(3, 3))
 
-        self.conv7 = self.conv_block(n=1, nIn=512, nOut=1024)
-        self.conv8 = self.conv_block(n=1, nIn=1024, nOut=2048)
-        self.pool9 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.conv7 = self.conv_block(n=1, nIn=512, nOut=2048)
+        self.pool9 = nn.MaxPool2d(kernel_size=(3, 3))
 
         # self.conv10 = self.conv_block(n=1, nIn=1024, nOut=1024)
         # self.conv11 = self.conv_block(n=1, nIn=1024, nOut=1024)
 
         self.flatten = nn.Flatten(1)
 
-        self.dense1 = self.dense_block(2, nIn=2048*7*7, nOut=1024)
+        self.dense1 = self.dense_block(2, nIn=2048*6*6, nOut=1024)
         self.dense2 = self.dense_block(2, nIn=1024, nOut=128)
         self.dense4 = self.dense_block(2, nIn=128, nOut=1)
 
@@ -44,15 +41,15 @@ class TomatoNN(nn.Module):
 
     def forward(self, X):
         X = self.conv1(X)
-        X = self.conv2(X)
+        # X = self.conv2(X)
         X = self.pool3(X)
 
         X = self.conv4(X)
-        X = self.conv5(X)
+        # X = self.conv5(X)
         X = self.pool6(X)
 
         X = self.conv7(X)
-        X = self.conv8(X)
+        # X = self.conv8(X)
         X = self.pool9(X)
 
         X = self.flatten(X)

0627d6e42bbd98e50a252f15c340a358f933662c
mahdi <mahdid.m.2000@gmail.com>
2023-03-02 16:08:19 +0330

    train exp - pool,dense

--- models/TomatoNN.py
+++ models/TomatoNN.py
@@ -7,24 +7,24 @@ class TomatoNN(nn.Module):
         super(TomatoNN, self).__init__()
         self.conv1 = self.conv_block(n=1, nIn=3, nOut=64)
         self.conv2 = self.conv_block(n=1, nIn=64, nOut=128)
-        self.pool3 = nn.MaxPool2d(kernel_size=(2,2))
+        self.pool3 = nn.MaxPool2d(kernel_size=(3,3))
 
         self.conv4 = self.conv_block(n=1, nIn=128, nOut=128)
         self.conv5 = self.conv_block(n=1, nIn=128, nOut=512)
-        self.pool6 = nn.MaxPool2d(kernel_size=(4, 4))
+        self.pool6 = nn.MaxPool2d(kernel_size=(3, 3))
 
         self.conv7 = self.conv_block(n=1, nIn=512, nOut=512)
         self.conv8 = self.conv_block(n=1, nIn=512, nOut=1024)
-        self.pool9 = nn.MaxPool2d(kernel_size=(2, 2))
+        # self.pool9 = nn.MaxPool2d(kernel_size=(2, 2))
 
         # self.conv10 = self.conv_block(n=1, nIn=1024, nOut=1024)
         # self.conv11 = self.conv_block(n=1, nIn=1024, nOut=1024)
 
         self.flatten = nn.Flatten(1)
 
-        self.dense1 = self.dense_block(2, nIn=1024*13*13, nOut=1024)
-        self.dense2 = self.dense_block(2, nIn=1024, nOut=128)
-        self.dense4 = self.dense_block(2, nIn=128, nOut=1)
+        self.dense1 = self.dense_block(2, nIn=1024*22*22, nOut=1024*32)
+        self.dense2 = self.dense_block(2, nIn=1024*32, nOut=128*8)
+        self.dense4 = self.dense_block(2, nIn=128*8, nOut=1)
 
     def conv_block(self, n, nIn, nOut, batch_norm=True, pad=0):
         block = nn.Sequential()
@@ -53,7 +53,7 @@ class TomatoNN(nn.Module):
 
         X = self.conv7(X)
         X = self.conv8(X)
-        X = self.pool9(X)
+        # X = self.pool9(X)
 
         X = self.flatten(X)
 
